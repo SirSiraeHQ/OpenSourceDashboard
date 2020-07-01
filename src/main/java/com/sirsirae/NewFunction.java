4d9c4b3318b7f4540e05f50c6b6bc1ff9a5f0f55
Olivier Alves <olivier.dasilvaalves@unil.ch>
2020-07-01 11:21:39 +0200

    Commit with BS

--- src/main/java/com/sirsirae/NewFunction.java
+++ src/main/java/com/sirsirae/NewFunction.java
@@ -2,8 +2,10 @@ package com.sirsirae;
 
 public class NewFunction {
 
+	public final String MYVARISCOOL="Thelo";
 
 	public int test(String hello) {
+		System.out.print(MYVARISCOOL);
 		return 3;
 	}
 
